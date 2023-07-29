9633a3760fd4c4bb82b74ce4019eb6dcf1016db5
Vasily Glazkov <vvg.doc@yandex.ru>
2023-07-29 22:43:58 +0200

    done 207. Linking between routes

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
+
+export default function Homepage() {
+	return (
+		<>
+			<PageNav />
+			<h1>WorldWise</h1>
+		</>
+	);
+}
---
+++ src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageNav from '../components/PageNav';
+
+export default function Pricing() {
+	return (
+		<>
+			<PageNav />
+			<h1>Pricing</h1>
+		</>
+	);
+}
